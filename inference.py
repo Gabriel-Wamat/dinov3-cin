d9ba8012d02ce39a1b09a799b552906831233a6f
Gabriel Wamat <gabrielwalisson54@gmail.com>
2025-10-01 19:25:13 -0300

    ajustes para conseguir roda a POC 1

--- inference.py
+++ inference.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), "dinov3"))
 import torch
 from torchvision import transforms
 from PIL import Image
